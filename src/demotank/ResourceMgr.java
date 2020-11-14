ee11d2a00840c032805b388191edc706cca669bf
ProGEvil <fl_cute@163.com>
2020-11-14 20:01:36 +0800

    优化了敌我坦克的资源图

--- src/demotank/ResourceMgr.java
+++ src/demotank/ResourceMgr.java
@@ -5,16 +5,23 @@ import java.awt.image.BufferedImage;
 import java.io.IOException;
 
 public class ResourceMgr {
-    public static BufferedImage tankD,tankL,tankR,tankU;
+    public static BufferedImage goodTankD,goodTankL,goodTankR,goodTankU;
+    public static BufferedImage badTankD,badTankL,badTankR,badTankU;
     public static BufferedImage bulletD,bulletL,bulletR,bulletU;
     public static BufferedImage[] exp = new BufferedImage[16];
 
     static {
         try {
-            tankU = ImageIO.read(ResourceMgr.class.getClassLoader().getResourceAsStream("images/BadTank1.png"));
-            tankL = ImageUtil.rotateImage(tankU,-90);
-            tankR = ImageUtil.rotateImage(tankU,90);
-            tankD = ImageUtil.rotateImage(tankU,180);
+            goodTankU = ImageIO.read(ResourceMgr.class.getClassLoader().getResourceAsStream("images/GoodTank1.png"));
+            goodTankL = ImageUtil.rotateImage(goodTankU,-90);
+            goodTankR = ImageUtil.rotateImage(goodTankU,90);
+            goodTankD = ImageUtil.rotateImage(goodTankU,180);
+
+            badTankU = ImageIO.read(ResourceMgr.class.getClassLoader().getResourceAsStream("images/BaDTank1.png"));
+            badTankL = ImageUtil.rotateImage(badTankU,-90);
+            badTankR = ImageUtil.rotateImage(badTankU,90);
+            badTankD = ImageUtil.rotateImage(badTankU,180);
+
 
             bulletU = ImageIO.read(ResourceMgr.class.getClassLoader().getResourceAsStream("images/bulletU.png"));
             bulletL = ImageUtil.rotateImage(bulletU,-90);
